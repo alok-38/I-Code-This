ccf955a436abf78e4cd3fe5a21bf01645f043ab5
Alokananda Y <plantsfortrees@gmail.com>
2024-12-20 01:26:26 +0000

    Not working on JS

---
+++ 30-Day-Roadmap/calculator/app.js
@@ -0,0 +1,34 @@
+// Header display
+const calculationEl = document.getElementById('calculation');
+const resultEl = document.getElementById('result');
+
+// functions
+const CEEl = document.getElementById('CE');
+const C = document.getElementById('C');
+const delEl = document.getElementById('del');
+
+// arithmetic
+const addEl = document.getElementById('add');
+const subtractEl = document.getElementById('subtract');
+const multiplyEl = document.getElementById('multiply');
+const divideEl = document.getElementById('divide');
+const equalsEl = document.getElementById('equals');
+const plusMinusEl = document.getElementById('plus-minus');
+
+// Numerals
+const periodEl = document.getElementById('period');
+const zeroEl = document.getElementById('zero');
+const oneEl = document.getElementById('one');
+const twoEl = document.getElementById('two');
+const threeEl = document.getElementById('three');
+const fourEl = document.getElementById('four');
+const fiveEl = document.getElementById('five');
+const sixEl = document.getElementById('six');
+const sevenEl = document.getElementById('seven');
+const eightEl = document.getElementById('eight');
+const nineEl = document.getElementById('nine');
+
+// Addition
+const add = () => {
+
+}
